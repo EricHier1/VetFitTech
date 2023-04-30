6d78d71e82b7c88a8aea9a9d8a041be9d877e926
EricHier1 <erichierholzer1@gmail.com>
2023-04-29 20:50:12 -0700

    changes to break down code into 3 different files

---
+++ str_gui.py
@@ -0,0 +1,4 @@
+from str_functions import create_gui
+
+if __name__ == "__main__":
+    create_gui()
---
+++ str_main.py
@@ -0,0 +1,5 @@
+from str_functions import main, create_gui
+
+if __name__ == "__main__":
+    create_gui()
+    # main()
